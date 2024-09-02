257a6a45d75d9fb4fb0c93b71f5ca72afd9d1bae
priyanshisaraogi <priyanshisaraogi4@gmail.com>
2024-09-02 16:22:56 +0800

    added generate mnemonic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [seed, setSeed] = useState("");
+  const [mnemonic, setMnemonic] = useState("");
+  function MyButton() {
+    return (
+      <button onClick={async function() {
+        const mn = await generateMnemonic();
+        setMnemonic(mn);
+        const seedBuffer= mnemonicToSeedSync(mn);
+        setSeed(seedBuffer.toString('hex'));
+      }}>
+        Create Seed Phrase
+        </button>
+      
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>PriWallet</h1>
+      <MyButton />
+      <p>{mnemonic}</p>
+    </div>
+  );
 }
 
 export default App
